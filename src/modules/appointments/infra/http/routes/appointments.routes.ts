6b7caf85d8cd378491827e3a2813521dfe130e25
Felipe Mendes <fmagliom@gmail.com>
2020-05-04 14:11:00 -0300

    Finished module

---
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { container } from 'tsyringe';
+
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+
+const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+appointmentsRouter.post('/', appointmentsController.create);
+
+export default appointmentsRouter;
